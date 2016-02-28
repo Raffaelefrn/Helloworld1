92c69e3a798e9646692599294eb93e31bdb8ffdb
Raffaele-PC <raffa.fari57@gmail.com>
2016-02-28 13:10:04 +0100

    Calcolo del volume e superficie con varibili float

--- sfera.py
+++ sfera.py
@@ -1,8 +1,8 @@
 pi=3.14
 raggio=input ('Inserisci il raggio:' )
 
-volume=(4*pi*raggio*raggio*raggio)/3
+volume=(4.00*pi*raggio*raggio*raggio)/3
 print"Volume: ", volume
 
-area=4*pi*raggio*raggio
+area=4.00*pi*raggio*raggio
 print"area della superficie: ",area
