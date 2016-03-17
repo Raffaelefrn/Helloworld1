0f6bd8967272327351e3fff60291b14826968565
Raffaele-PC <raffa.fari57@gmail.com>
2016-03-17 16:33:33 +0100

    Sfera,con virgola mobile

--- sfera.py
+++ sfera.py
@@ -1,8 +1,8 @@
 pi=3.14
 raggio=input ('Inserisci il raggio:' )
 
-volume=(4.00*pi*raggio*raggio*raggio)/3.00
+volume=(4.0*pi*raggio*raggio*raggio)/3.0
 print"Volume: ", volume
 
-area=4.00*pi*raggio*raggio
+area=4.0*pi*raggio*raggio
 print"area della superficie: ",area
